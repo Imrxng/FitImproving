831b6a284aa94b3d139b55e1eea5a1ff24e424d9
imran.ghaddoura <s148286@ap.be>
2024-05-31 13:31:41 +0000

    terug site online gezet zonder workflow voor cloud

--- Deelproject-06/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-$databaseHost = 'db';
-$databaseUsername = 'root';
-$databasePassword = 'wachtwoord';
-$databaseName = 'bezoekGeschiedenis';
-
-$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
-?>
-    
--- Dockerfile
+++
@@ -1,8 +0,0 @@
-FROM php:apache 
-
-COPY  ./Deelproject-06/ /usr/local/apache2/htdocs/
-
-EXPOSE 80
-
-RUN docker-php-ext-install mysqli && apt-get update
-RUN cp -r /usr/local/apache2/htdocs/* /var/www/html/
